5df50ec7bb266180ce503fc362945aa1ed873f18
felix-wang-0307 <hwang779@usc.edu>
2024-10-22 19:03:00 -0700

    feat: update the frontend to simulate a call to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const backend = "https://weather-search-web-571.wn.r.appspot.com/";
 
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch(backend + "test?param1=Hello&param2=World")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(JSON.stringify(data.params));
+        setData(JSON.stringify(data.params));  // Convert the object to a string
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello World From React 18!</h1>
+      <h1>Hello World From React 18! Shit!!</h1>
+      {data ? <h2>{data}</h2> : <p>Loading...</p>}
     </div>
   );
 }
