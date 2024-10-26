849fdfbd55bd4cff9e1087737ae08d6ab3da88bb
felix-wang-0307 <hwang779@usc.edu>
2024-10-25 17:30:43 -0700

    feat: implement some service functions in service.ts and add a weather controller

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,41 +1,32 @@
 import express from 'express';
 import cors from 'cors';  // Import the CORS package
 import * as dotenv from 'dotenv';
-import { run } from './mongodb';
+import { fetchWeatherData } from "./service";
 
 const FRONTEND_URL = 'https://weather-search-frontend-571.wm.r.appspot.com';
 const BACKEND_URL = 'https://weather-search-web-571.wn.r.appspot.com';
 
 dotenv.config();
 
-run(process.env.MONGODB_URI).then(() => {
-    console.log('Connected to MongoDB');
-});
-
 const app = express();
 
-// Use CORS middleware to allow cross-origin requests
 app.use(cors());
-
-// Example: Restrict CORS to specific frontend URL (change to your frontend URL)
 app.use(cors({
-    origin: FRONTEND_URL
+  origin: FRONTEND_URL
 }));
 
 app.get('/', (req, res) => {
-    res.send('Hello from App Engine!');
+  res.send('Hello from App Engine!');
 });
 
-app.get('/test', (req, res) => {
-    const params = req.query;
-    res.json({
-        message: 'Hello from App Engine!',
-        params
-    });
+app.get('/weather', async (req, res) => {
+    const { latitude , longitude } = req.query;
+    const data = await fetchWeatherData(latitude, longitude);
+    res.json(data);
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`);
+  console.log(`Server listening on port ${PORT}...`);
 });
---
+++ backend/src/service.ts
@@ -0,0 +1,110 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const WEATHER_API = 'https://api.tomorrow.io/v4/timelines';
+const GOOGLE_MAPS_API = 'https://maps.googleapis.com/maps/api/geocode/json';
+
+interface IResponse {
+  success: boolean;
+  data?: any;
+  error?: string;
+  statusCode?: number;
+}
+
+export async function fetchWeatherData(latitude: string, longitude: string): Promise<IResponse> {
+  console.log('Incoming request for weather data:', latitude, longitude);
+
+  const apiKey = process.env.TOMORROW_API_KEY || '';
+  const params = new URLSearchParams({
+    location: `${latitude},${longitude}`,
+    apikey: apiKey,
+    fields: [
+      'temperature',
+      'temperatureApparent',
+      'temperatureMin',
+      'temperatureMax',
+      'windSpeed',
+      'windDirection',
+      'humidity',
+      'pressureSeaLevel',
+      'uvIndex',
+      'weatherCode',
+      'precipitationProbability',
+      'precipitationType',
+      'sunriseTime',
+      'sunsetTime',
+      'visibility',
+      'moonPhase',
+      'cloudCover',
+    ].join(','),
+    units: 'imperial',
+    timesteps: ['current', '1h', '1d'].join(','),
+    timezone: 'America/Los_Angeles',
+  });
+
+  try {
+    const response = await fetch(`${WEATHER_API}?${params}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch weather data from primary API key');
+    }
+    const data = await response.json();
+    return { success: true, data: data.data } ;
+  } catch (error) {
+    console.error('Failed to fetch weather data from primary API key, trying failover', error);
+    const failoverApiKey = process.env.TOMORROW_API_FAILOVER_KEY || '';
+    params.set('apikey', failoverApiKey);
+
+    try {
+      const response = await fetch(`${WEATHER_API}?${params}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch weather data from failover API key');
+      }
+      const data = await response.json();
+      return { success: true, data: data.data } ;
+    } catch (failoverError) {
+      console.error('Failed to fetch weather data from failover API key', failoverError);
+      return { success: false, error: 'Failed to fetch weather data' };
+    }
+  }
+}
+
+export async function getGeocodeInfo(address: string): Promise<IResponse> {
+  try {
+    const params = new URLSearchParams({
+      address,
+      key: process.env.GOOGLE_MAPS_API_KEY || '',
+    });
+    const response = await fetch(`${GOOGLE_MAPS_API}?${params}`, {
+      headers: {
+        'Accept-Language': 'en',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch geocode information');
+    }
+
+    const data = await response.json();
+    const results = data.results;
+    if (results && results.length > 0) {
+      const result = results[0];
+      const coordinates = result.geometry.location;
+      const formattedAddress = result.formatted_address;
+
+      return {
+        statusCode: 200,
+        success: true,
+        data: {
+          latitude: coordinates.lat,
+          longitude: coordinates.lng,
+          formatted_address: formattedAddress,
+        },
+      };
+    } else {
+      return { statusCode: 500, success: false, error: 'Failed to fetch address info'};
+    }
+  } catch (error) {
+    console.error('Error fetching geocode information:', error);
+    return { statusCode: 500, success: false, error: 'Failed to fetch address info' };
+  }
+}
