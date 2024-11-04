51e0bc8e097c015ceb8d6dde50f1555ef1b27a16
felix-wang-0307 <hwang779@usc.edu>
2024-11-04 15:42:58 -0800

    chore: update the dependencies and env files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import WeatherSearchForm from './components/weatherSearchForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const backend = "https://weather-search-web-571.wn.r.appspot.com/";
 
