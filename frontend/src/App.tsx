0a9c6c4cd15c575e9f6aba508f34e9d54bd731e5
felix-wang-0307 <hwang779@usc.edu>
2024-11-04 21:47:24 -0800

    feat: implement an initial view of the weather form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import WeatherSearchForm from './components/weatherSearchForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
@@ -8,7 +8,7 @@ const backend = "https://weather-search-web-571.wn.r.appspot.com/";
 function App() {
   return (
     <div className="App">
-      <WeatherSearchForm />
+      <WeatherSearchForm onSubmit={() => {}}/>
     </div>
   );
 }
--- frontend/src/components/weatherSearchForm.tsx
+++ frontend/src/components/weatherSearchForm.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import "./weatherSearchForm.scss";
+import states from "./states.json";
 
-const WeatherSearchForm = () => {
+const WeatherSearchForm = ({ onSubmit }) => {
+	const [stateList, setStateList] = useState<React.JSX.Element[]>([]);
+
+	useEffect(() => {
+		const stateOptions = Object.entries(states).map(([name, abbreviation]) => (
+			<option key={abbreviation} value={abbreviation}>
+				{name}
+			</option>
+		));
+		setStateList(stateOptions);
+	}, []);
+
 	return (
-		<Container className="mt-4 ps-4 pe-4 weather-search-form-container">
+		<Container className="mt-4 weather-search-form-container">
 			<div className="weather-search-title mt-2 mb-4">
 				<h2>Weather Search 🌤️</h2>
 			</div>
-			<Form className="weather-search-form">
-				{/* Street Field */}
-				<Form.Group as={Row} className="mb-3" controlId="formStreet">
-					<Form.Label as={Col} sm={2} xs={12}>
+			<Form className="weather-search-form" onSubmit={onSubmit}>
+				<Form.Group as={Row} className="mb-1" controlId="formStreet">
+					<Form.Label column sm={2} xs={12} className="required">
 						Street
 					</Form.Label>
 					<Col sm={10} xs={12}>
@@ -19,9 +30,8 @@ const WeatherSearchForm = () => {
 					</Col>
 				</Form.Group>
 
-				{/* City Field */}
-				<Form.Group as={Row} className="mb-3" controlId="formCity">
-					<Form.Label column sm={2} xs={12}>
+				<Form.Group as={Row} className="mb-1" controlId="formCity">
+					<Form.Label column sm={2} xs={12} className="required">
 						City
 					</Form.Label>
 					<Col sm={10} xs={12}>
@@ -29,23 +39,20 @@ const WeatherSearchForm = () => {
 					</Col>
 				</Form.Group>
 
-				{/* State Field */}
-				<Form.Group as={Row} className="mb-3" controlId="formState">
-					<Form.Label column sm={2} xs={12}>
+				<Form.Group as={Row} className="mb-1" controlId="formState">
+					<Form.Label column sm={2} xs={12} className="required">
 						State
 					</Form.Label>
-					<Col sm={10} xs={12}>
-						<Form.Control as="select" required>
-							<option>Select your state</option>
-							<option>California</option>
-							<option>Texas</option>
-							<option>New York</option>
-							{/* Add other states as needed */}
+					<Col sm={10} xs={12} className="w-50 state-select">
+						<Form.Control as="select" required defaultValue="">
+							<option value="" disabled>Select your state</option>
+							{ stateList }
 						</Form.Control>
 					</Col>
 				</Form.Group>
 
-				{/* Autodetect and Current Location Checkboxes */}
+				<div className="split-line"></div>
+
 				<Form.Group as={Row} className="mb-3" controlId="formOptions">
 					<Col sm={{ span: 10, offset: 2 }} xs={12}>
 						<Form.Check type="checkbox" label="Autodetect Location" />
