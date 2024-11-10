df6fd3016f40cf011547b1192f5272af95515b27
felix-wang-0307 <hwang779@usc.edu>
2024-11-10 04:09:07 -0800

    feat: restructure App component layout; add TopTabButtons and update styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,34 @@
-import React, { useCallback, FormEvent } from "react";
+import React, { useCallback, FormEvent, useRef } from "react";
 import WeatherSearchForm from "./components/weatherSearchForm";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
-import { IFormData } from "./types";
+import { fetchData } from "./model/fetchData";
+import TopTabButtons from "./components/topTabButtons";
 
 const BACKEND = "https://weather-search-web-571.wn.r.appspot.com/";
 console.log(BACKEND);
 
 function App() {
-  const submitForm = useCallback(((event: FormEvent) => {
+  const submitForm = useCallback((event: FormEvent) => {
     event.preventDefault();
-    const formData: IFormData = {
+    const formData = {
       street: event.target[0].value,
       city: event.target[1].value,
       state: event.target[2].value,
       autoDetect: event.target[3].checked,
     };
-    console.log(formData);
-  }), []);
+    fetchData(formData).then((data) => {
+      console.log(data);
+    });
+  }, []);
+
+  const topTabButtonsRef = useRef(null);
 
   return (
-    <div className="App">
-      <WeatherSearchForm onSubmit={ submitForm } />
-    </div>
+    <article className="App">
+      <WeatherSearchForm onSubmit={submitForm} />
+      <TopTabButtons ref={topTabButtonsRef}/>
+    </article>
   );
 }
 
