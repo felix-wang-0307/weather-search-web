672ef16e62d78c4c6c7ba8c20568f84205cc89a0
felix-wang-0307 <hwang779@usc.edu>
2024-11-09 16:20:06 -0800

    feat: enhance WeatherSearchForm with state selection and validation for street and city inputs

--- frontend/src/components/weatherSearchForm.tsx
+++ frontend/src/components/weatherSearchForm.tsx
@@ -1,79 +1,108 @@
-import React, { useState, useEffect } from 'react';
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import React, { useState, useEffect, useMemo, useRef } from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import "./weatherSearchForm.scss";
+import { Autocomplete } from "@mui/material";
 import states from "./states.json";
 
 const WeatherSearchForm = ({ onSubmit }) => {
-	const [stateList, setStateList] = useState<React.JSX.Element[]>([]);
+  const [isStreetValid, setIsStreetValid] = useState(true);
+  const [isCityValid, setIsCityValid] = useState(true);
+  return (
+    <Container className="mt-4 weather-search-form-container">
+      <div className="weather-search-title mt-2 mb-4">
+        <h2>Weather Search ⛅</h2>
+      </div>
+      <Form className="weather-search-form" onSubmit={onSubmit}>
+        <Form.Group as={Row} className="mb-1" controlId="formStreet">
+          <Form.Label column sm={2} xs={12} className="required">
+            Street
+          </Form.Label>
+          <Col sm={10} xs={12}>
+            <Form.Control
+              type="text"
+              required
+              onBlur={(e) => {
+                setIsStreetValid(e.target.value.trim() !== "");
+              }}
+              isInvalid={!isStreetValid}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid street.
+            </Form.Control.Feedback>
+          </Col>
+        </Form.Group>
 
-	useEffect(() => {
-		const stateOptions = Object.entries(states).map(([name, abbreviation]) => (
-			<option key={abbreviation} value={abbreviation}>
-				{name}
-			</option>
-		));
-		setStateList(stateOptions);
-	}, []);
+        <Form.Group as={Row} className="mb-1" controlId="formCity">
+          <Form.Label column sm={2} xs={12} className="required">
+            City
+          </Form.Label>
+          <Col sm={10} xs={12}>
+            <Form.Control
+              type="text"
+              required
+              onBlur={(e) => {
+                setIsCityValid(e.target.value.trim() !== "");
+              }}
+              isInvalid={!isCityValid}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid city.
+            </Form.Control.Feedback>
+          </Col>
+        </Form.Group>
 
-	return (
-		<Container className="mt-4 weather-search-form-container">
-			<div className="weather-search-title mt-2 mb-4">
-				<h2>Weather Search 🌤️</h2>
-			</div>
-			<Form className="weather-search-form" onSubmit={onSubmit}>
-				<Form.Group as={Row} className="mb-1" controlId="formStreet">
-					<Form.Label column sm={2} xs={12} className="required">
-						Street
-					</Form.Label>
-					<Col sm={10} xs={12}>
-						<Form.Control type="text" placeholder="Enter street" required />
-					</Col>
-				</Form.Group>
+        <Form.Group as={Row} className="mb-1" controlId="formState">
+          <Form.Label column sm={2} xs={12} className="required">
+            State
+          </Form.Label>
+          <Col sm={10} xs={12}>
+            <Autocomplete
+              freeSolo
+							autoComplete={false}
+              id="state-select"
+              disableClearable
+              options={Object.keys(states)}
+              renderInput={(params) => {
+								return (
+									<div ref={params.InputProps.ref} className="">
+										<input {...params.inputProps} type="text" className="form-control"/>
+									</div>
+								)
+							}}
+              renderOption={(props, option) => {
+                const { key, ...rest } = props;
+                return (
+                  <li key={key} {...rest}>
+                    {option}
+                  </li>
+                );
+              }}
+            />
+          </Col>
+        </Form.Group>
 
-				<Form.Group as={Row} className="mb-1" controlId="formCity">
-					<Form.Label column sm={2} xs={12} className="required">
-						City
-					</Form.Label>
-					<Col sm={10} xs={12}>
-						<Form.Control type="text" placeholder="Enter city" required />
-					</Col>
-				</Form.Group>
+        <div className="split-line"></div>
 
-				<Form.Group as={Row} className="mb-1" controlId="formState">
-					<Form.Label column sm={2} xs={12} className="required">
-						State
-					</Form.Label>
-					<Col sm={10} xs={12} className="w-50 state-select">
-						<Form.Control as="select" required defaultValue="">
-							<option value="" disabled>Select your state</option>
-							{ stateList }
-						</Form.Control>
-					</Col>
-				</Form.Group>
+        <Form.Group as={Row} className="mb-3" controlId="formOptions">
+          <Col sm={{ span: 10, offset: 2 }} xs={12}>
+            <Form.Check type="checkbox" label="Autodetect Location" />
+          </Col>
+        </Form.Group>
 
-				<div className="split-line"></div>
-
-				<Form.Group as={Row} className="mb-3" controlId="formOptions">
-					<Col sm={{ span: 10, offset: 2 }} xs={12}>
-						<Form.Check type="checkbox" label="Autodetect Location" />
-						<Form.Check type="checkbox" label="Current Location" className="mt-1" />
-					</Col>
-				</Form.Group>
-
-				{/* Buttons */}
-				<Form.Group as={Row} className="mb-3">
-					<Col sm={{ span: 10, offset: 2 }} xs={12}>
-						<Button variant="primary" type="submit" className="me-2">
-							Search
-						</Button>
-						<Button variant="secondary" type="reset">
-							Clear
-						</Button>
-					</Col>
-				</Form.Group>
-			</Form>
-		</Container>
-	);
+        {/* Buttons */}
+        <Form.Group as={Row} className="mb-3">
+          <Col sm={{ span: 10, offset: 2 }} xs={12}>
+            <Button variant="primary" type="submit" className="me-2">
+              Search
+            </Button>
+            <Button variant="secondary" type="reset">
+              Clear
+            </Button>
+          </Col>
+        </Form.Group>
+      </Form>
+    </Container>
+  );
 };
 
 export default WeatherSearchForm;
