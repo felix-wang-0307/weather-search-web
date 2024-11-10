cd306c6b076342fdc4d8f5ced66e5c09bf487ef4
felix-wang-0307 <hwang779@usc.edu>
2024-11-09 16:20:45 -0800

    chore: remove unfinished useWeatherData hook

--- frontend/src/hooks/useWeatherData.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck since this is unfinished code
-import { useEffect, useState } from 'react';
-
-export default function useWeatherData () {
-	const weatherData = useState({});
-	
-
-	return weatherData;
-}
