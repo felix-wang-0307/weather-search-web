d875edad414475f7d124570171428a75009a85c2
felix-wang-0307 <hwang779@usc.edu>
2024-11-11 23:36:25 -0800

    feat: implement active tab management and integrate AppContext for form data state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,30 @@
-import React, { useCallback, FormEvent, useRef } from "react";
+import React, {
+  useCallback,
+  FormEvent,
+  useRef,
+  useState
+} from "react";
+import { AppContext } from "./appContext";
 import WeatherSearchForm from "./components/weatherSearchForm";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 import { fetchData } from "./model/fetchData";
 import TopTabButtons from "./components/topTabButtons";
+import { IFormData, ITabRef } from "./types";
+import ResultTab from "./components/resultTab";
+import FavoriteTab from "./components/favoriteTab";
 
 const BACKEND = "https://weather-search-web-571.wn.r.appspot.com/";
 console.log(BACKEND);
 
 function App() {
+  const [activeTab, setActiveTab] = useState("results");
+  const topTabButtonsRef = useRef<ITabRef>(null);
+  const [formData, setFormData] = useState<IFormData>({});
+  const [searchStatus, setSearchStatus] = useState<"init" | "success" | "fail">(
+    "init"
+  );
+
   const submitForm = useCallback((event: FormEvent) => {
     event.preventDefault();
     const formData = {
@@ -17,18 +33,37 @@ function App() {
       state: event.target[2].value,
       autoDetect: event.target[3].checked,
     };
-    fetchData(formData).then((data) => {
-      console.log(data);
-    });
+    fetchData(formData)
+      .then((data) => {
+        const refinedSearchData = {
+          ...formData,
+          city: data.city,
+          state: data.state,
+        }
+        setFormData(refinedSearchData);
+        setSearchStatus("success");
+        console.log(data);
+      })
+      .catch(() => {
+        setSearchStatus("fail");
+      });
   }, []);
 
-  const topTabButtonsRef = useRef(null);
+  const handleTabChange = useCallback((tab: string) => {
+    setActiveTab(tab);
+  }, []);
 
   return (
-    <article className="App">
-      <WeatherSearchForm onSubmit={submitForm} />
-      <TopTabButtons ref={topTabButtonsRef}/>
-    </article>
+    <AppContext.Provider value={formData}>
+      <article className="App">
+        <WeatherSearchForm onSubmit={submitForm} />
+        <TopTabButtons ref={topTabButtonsRef} onTabChange={handleTabChange} />
+        <section className="mt-3">
+        {searchStatus === "success" &&
+          (activeTab === "results" ? <ResultTab searchStatus={searchStatus}/> : <FavoriteTab />)}
+        </section>
+      </article>
+    </AppContext.Provider>
   );
 }
 
