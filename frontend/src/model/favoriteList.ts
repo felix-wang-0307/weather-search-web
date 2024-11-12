7501a268c30f63933a1507359a44cab751092049
felix-wang-0307 <hwang779@usc.edu>
2024-11-12 01:45:47 -0800

    feat: implement favorite management functions for fetching, adding, and deleting favorites

--- frontend/src/model/favoriteList.ts
+++ frontend/src/model/favoriteList.ts
@@ -1,3 +1,38 @@
-// @ts-ignore
+import { ICityInfo } from "@/types";
+import { getUserId } from "@/utils";
 
-export {};
+const BACKEND = "https://weather-search-web-571.wn.r.appspot.com/";
+
+export async function getFavorites(): Promise<ICityInfo[]> {
+  const userId = getUserId();
+  const url = `${BACKEND}favorites?user=${userId}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.data;
+}
+
+export async function addFavorite({ city, state }: ICityInfo): Promise<boolean> {
+  const userId = getUserId();
+  const url = `${BACKEND}favorite`;
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ user: userId, city, state }),
+  });
+  return response.ok;
+}
+
+export async function deleteFavorite({ city, state }: ICityInfo): Promise<boolean> {
+  const userId = getUserId();
+  const url = `${BACKEND}favorite`;
+  const response = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ user: userId, city, state }),
+  });
+  return response.ok;
+}
