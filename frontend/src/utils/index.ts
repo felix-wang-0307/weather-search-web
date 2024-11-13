7b0d56801d4e3946ee86642149a4cc0eae57df5e
felix-wang-0307 <hwang779@usc.edu>
2024-11-12 22:51:14 -0800

    feat: add convertDate utility function for formatting ISO date strings

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -19,6 +19,15 @@ export function parseJsonSafely(json: string): any {
   }
 }
 
+export function convertDate(ISODate: string): string {
+  const date = new Date(ISODate);
+  // Transfer the ISODate to "Wednesday, 1 Jan 2020" format
+  const localeString = date.toLocaleDateString("en-US", {weekday: "long", day: "2-digit", month: "short", year: "numeric"});
+  // Remove the 2nd comma between the Month and Year
+  const secondCommaIndex = localeString.indexOf(",", localeString.indexOf(",") + 1);
+  return localeString.slice(0, secondCommaIndex) + localeString.slice(secondCommaIndex + 1);
+}
+
 export const getUserId = () => {
   let userId = localStorage.getItem('userId');
   // If no user ID is found, generate a new one and store it
