9d553383cf45710d347ca7c751b10520a52d0793
felix-wang-0307 <hwang779@usc.edu>
2024-11-12 21:11:03 -0800

    feat: implement the front-end of Favorite List and its styling

--- frontend/src/components/favoriteTab/index.tsx
+++ frontend/src/components/favoriteTab/index.tsx
@@ -1,10 +1,105 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Alert, Row, Col } from "react-bootstrap";
+import { getFavorites } from "../../model/favoriteList";
+import { ICityInfo } from "@/types";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./index.scss";
 
-export default function FavoriteTab() {
+interface IFavoriteTabProps {
+  onClickFavorite: (city: string, state: string) => Promise<void>;
+  onDeleteFavorite: (city: string, state: string) => Promise<void>;
+}
+
+type IFavoriteTableProps = IFavoriteTabProps & {
+  favorites: ICityInfo[];
+};
+
+const FavoriteTable = ({
+  favorites,
+  onClickFavorite,
+  onDeleteFavorite,
+}: IFavoriteTableProps) => {
+  // the table styles are defined in the App.scss file
   return (
-    <Container className="d-flex justify-content-center">
-      <h1>Favorite Tab</h1>
+    <Container>
+      <Row className="table-header">
+        <Col xs={2}>#</Col>
+        <Col xs={4}>City</Col>
+        <Col xs={4}>State</Col>
+        <Col xs={4}></Col>
+      </Row>
+      {favorites.map((favorite: ICityInfo, index: number) => (
+        <Row className="table-row" key={index}>
+          <Col xs={2}>{index + 1}</Col>
+          <Col
+            xs={4}
+            className="link"
+            onClick={() => {
+              onClickFavorite(favorite.city, favorite.state);
+            }}
+          >
+            {favorite.city}
+          </Col>
+          <Col
+            xs={4}
+            className="link"
+            onClick={() => {
+              onClickFavorite(favorite.city, favorite.state);
+            }}
+          >
+            {favorite.state}
+          </Col>
+          <Col xs={2}>
+            <i
+              className="bi bi-trash-fill"
+              onClick={() => {
+                onDeleteFavorite(favorite.city, favorite.state);
+              }}
+              onMouseOver={(e) => {
+                e.currentTarget.className = "bi bi-trash";
+              }}
+              onMouseLeave={(e) => {
+                e.currentTarget.className = "bi bi-trash-fill";
+              }}
+            ></i>
+          </Col>
+        </Row>
+      ))}
+    </Container>
+  );
+};
+
+export default function FavoriteTab({
+  onClickFavorite,
+  onDeleteFavorite,
+}: IFavoriteTabProps) {
+  const [favorites, setFavorites] = useState<ICityInfo[]>([]);
+
+  const fetchFavorites = () => {
+    getFavorites().then((favorites) => {
+      setFavorites(favorites);
+    });
+  };
+
+  useEffect(fetchFavorites, []);
+
+  const handleDeleteFavorite = async (city: string, state: string) => {
+    await onDeleteFavorite(city, state).then(() => fetchFavorites());
+  };
+
+  return (
+    <Container>
+      {favorites.length === 0 ? (
+        <Alert variant="warning">
+          <b>Sorry! </b>No records found.
+        </Alert>
+      ) : (
+        <FavoriteTable
+          favorites={favorites}
+          onClickFavorite={onClickFavorite}
+          onDeleteFavorite={handleDeleteFavorite}
+        />
+      )}
     </Container>
   );
 }
