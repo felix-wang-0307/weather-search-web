8dbbd6b48a01ec5366456fe41d282450b247aea0
felix-wang-0307 <hwang779@usc.edu>
2024-11-13 14:02:51 -0800

    feat: add animations for content transitions in ResultTab and implement navigation in ResultDetails

--- frontend/src/components/resultTab/index.tsx
+++ frontend/src/components/resultTab/index.tsx
@@ -3,21 +3,39 @@ import { ResultHeader } from "./resultHeader";
 import { Container } from "react-bootstrap";
 import ResultContent from "./resultContent";
 import ResultDetails from "./resultDetails";
+import "./index.scss";
 
 export default function ResultTab() {
   const [page, setPage] = useState<"content" | "details">("content");
+  const [animation, setAnimation] = useState("");
+
+  const goToDetails = () => {
+    setAnimation("slide-out-left");
+    setTimeout(() => {
+      setPage("details");
+      setAnimation("slide-in-right");
+    }, 300);
+  };
+
+  const goBackToContent = () => {
+    setAnimation("slide-out-right");
+    setTimeout(() => {
+      setPage("content");
+      setAnimation("slide-in-left");
+    }, 300);
+  };
 
   return (
     <Container>
-      {page === "content" && (
-        <>
-          <ResultHeader goToDetails={() => setPage("details")}/>
+      <div>
+        <div className={page === "content" ? animation : "hide"}>
+          <ResultHeader goToDetails={goToDetails} />
           <ResultContent />
-        </>
-      )}
-      {page === "details" && (
-        <ResultDetails goBackToContent={() => setPage("content")}/>
-      )}
+        </div>
+        <div className={page === "details" ? animation : "hide"}>
+          <ResultDetails goBackToContent={goBackToContent} />
+        </div>
+      </div>
     </Container>
   );
 }
--- frontend/src/components/resultTab/resultDetails/index.tsx
+++ frontend/src/components/resultTab/resultDetails/index.tsx
@@ -1,3 +1,12 @@
-export default function ResultDetails ({ goBackToContent }) {
-  return <></>
+import React from "react";
+import { Button, Container } from "react-bootstrap";
+
+export default function ResultDetails({ goBackToContent }) {
+  return (
+    <Container>
+      <Button onClick={goBackToContent}>
+        <i className="bi bi-chevron-left"></i>List
+      </Button>
+    </Container>
+  );
 }
