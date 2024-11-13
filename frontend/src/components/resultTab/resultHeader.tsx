b52c591debf34de49bc7400900837ea7c0b48f33
felix-wang-0307 <hwang779@usc.edu>
2024-11-12 19:52:08 -0800

    feat: implement favorite toggle functionality and enhance ResultHeader styling

--- frontend/src/components/resultTab/resultHeader.tsx
+++ frontend/src/components/resultTab/resultHeader.tsx
@@ -1,19 +1,81 @@
 import { AppContext } from "../../appContext";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Button, Container } from "react-bootstrap";
+import {
+  getFavorites,
+  addFavorite,
+  deleteFavorite,
+} from "../../model/favoriteList";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "./resultHeader.scss";
+import { ICityInfo } from "@/types";
 
 export const ResultHeader = () => {
   const { city, state } = useContext(AppContext);
+  const [isFavorite, setIsFavorite] = React.useState(false);
+
+  useEffect(() => {
+    getFavorites().then((favorites) => {
+      setIsFavorite(
+        favorites.some(
+          (favorite) => favorite.city === city && favorite.state === state
+        )
+      );
+    });
+  }, [city, state]);
+
+  const handleAddFavorite = () => {
+    addFavorite({ city, state } as ICityInfo)
+      .then(() => setIsFavorite(true))
+      .catch(() => {
+        alert("Failed to add favorite");
+      });
+  };
+
+  const handleDeleteFavorite = () => {
+    deleteFavorite({ city, state } as ICityInfo)
+      .then(() => setIsFavorite(false))
+      .catch(() => {
+        alert("Failed to delete favorite");
+      });
+  };
+
+  const toggleFavorite = () => {
+    if (isFavorite) {
+      handleDeleteFavorite();
+    } else {
+      handleAddFavorite();
+    }
+  };
+
   return (
     <Container>
       <Container className="d-flex justify-content-center">
-        <h3>Forecast at {city}, {state}</h3>
+        <h3 className="title">
+          Forecast at {city}, {state}
+        </h3>
       </Container>
       <Container className="mt-3 d-flex justify-content-end">
-        <Button variant="secondary" className="white-button"><i className="bi bi-star"></i></Button>
-        <span>Details</span>
+        <Button
+          variant="secondary"
+          className={`white-button favorite ${
+            isFavorite ? "is-favorite" : "not-favorite"
+          }`}
+          onClick={toggleFavorite}
+        >
+          {!isFavorite && <i className="bi bi-star"></i>}
+          {isFavorite && (
+            <i
+              className="bi bi-star-fill"
+              style={{ color: "yellow", WebkitTextStroke: "1px #000" }}
+            ></i>
+          )}
+        </Button>
+        <Button variant="light" className="go-to-detail">
+          <span>Details</span>
+          <i className="bi bi-chevron-right"></i>
+        </Button>
       </Container>
     </Container>
-  )
+  );
 };
