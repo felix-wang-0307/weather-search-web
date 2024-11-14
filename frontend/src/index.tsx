6fbd60b2c1d4ffc15b204861467da3c808e9065e
felix-wang-0307 <hwang779@usc.edu>
2024-11-13 16:37:17 -0800

    feat: integrate Google Maps API provider and update dependencies for enhanced map functionality

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { APIProvider } from "@vis.gl/react-google-maps";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+fetch("https://weather-search-web-571.wn.r.appspot.com/google-maps-api-key")
+  .then((res) => res.json())
+  .then(({ apiKey }) => {
+    root.render(
+      <React.StrictMode>
+        <APIProvider apiKey={apiKey}>
+          <App />
+        </APIProvider>
+      </React.StrictMode>
+    );
+  })
+  .catch((error) => {
+    console.error("Failed to fetch Google Maps API key", error);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  });
