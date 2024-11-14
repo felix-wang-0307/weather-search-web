b9fbed32bd9dd482fe1f540e94062db1a8447c57
felix-wang-0307 <hwang779@usc.edu>
2024-11-14 00:27:51 -0800

    feat: implement SlideContext for navigation between content and details in ResultTab component

--- frontend/src/components/resultTab/index.tsx
+++ frontend/src/components/resultTab/index.tsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 import { ResultHeader } from "./resultHeader";
 import { Container } from "react-bootstrap";
 import ResultContent from "./resultContent";
 import ResultDetails from "./resultDetails";
 import "./index.scss";
+
+export const SlideContext = createContext<{
+  goToDetails: (date: string) => void;
+  goBackToContent: () => void;
+  detailDate: string;
+}>({} as any);
 
 export default function ResultTab() {
   const [page, setPage] = useState<"content" | "details">("content");
@@ -28,14 +34,16 @@ export default function ResultTab() {
   };
 
   return (
-    <Container>
-      <div className={page === "content" ? animation : "hide"}>
-        <ResultHeader goToDetails={(date) => goToDetails(date)} />
-        <ResultContent />
-      </div>
-      <div className={page === "details" ? animation : "hide"}>
-        <ResultDetails goBackToContent={goBackToContent} detailDate={detailDate}/>
-      </div>
-    </Container>
+    <SlideContext.Provider value={{ goToDetails, goBackToContent, detailDate }}>
+      <Container>
+        <div className={page === "content" ? animation : "hide"}>
+          <ResultHeader goToDetails={(date) => goToDetails(date)} />
+          <ResultContent />
+        </div>
+        <div className={page === "details" ? animation : "hide"}>
+          <ResultDetails goBackToContent={goBackToContent} detailDate={detailDate}/>
+        </div>
+      </Container>
+    </SlideContext.Provider>
   );
 }
--- frontend/src/components/resultTab/resultContent/dayView.tsx
+++ frontend/src/components/resultTab/resultContent/dayView.tsx
@@ -4,34 +4,55 @@ import React, { useContext } from "react";
 import { Container, Row, Col, Alert } from "react-bootstrap";
 import { convertDate } from "../../../utils";
 import weatherCodes from "../../../data/weatherCodes.json";
+import { SlideContext } from "..";
 
 const DayViewTable = ({
   weeklyWeather,
 }: {
   weeklyWeather: IWeatherDetails[];
 }) => {
+  const { goToDetails, goBackToContent } = useContext(SlideContext);
   return (
     <Container className="table">
       <Row className="table-header">
         <Col xs={1}>#</Col>
-        <Col sm={3} xs={2}>Date</Col>
-        <Col sm={2} xs={3}>Status</Col>
+        <Col sm={3} xs={2}>
+          Date
+        </Col>
+        <Col sm={2} xs={3}>
+          Status
+        </Col>
         <Col xs={2}>Temp. High(°F)</Col>
         <Col xs={2}>Temp. Low(°F)</Col>
         {/* If it is xs-sized-device, show "Wind Speed" without (mph) */}
-        <Col sm={2} className="d-none d-sm-block">Wind Speed(mph)</Col>
-        <Col xs={2} className="d-block d-sm-none">Wind Speed</Col>
+        <Col sm={2} className="d-none d-sm-block">
+          Wind Speed(mph)
+        </Col>
+        <Col xs={2} className="d-block d-sm-none">
+          Wind Speed
+        </Col>
       </Row>
       {weeklyWeather.map((day: IWeatherDetails, index: number) => (
         <Row className="table-row" key={index}>
           <Col xs={1}>{index + 1}</Col>
-          <Col sm={3} xs={2} className="link">{convertDate(day.startTime)}</Col>
+          <Col
+            sm={3}
+            xs={2}
+            className="link"
+            onClick={() => goToDetails(day.startTime)}
+          >
+            {convertDate(day.startTime)}
+          </Col>
           <Col sm={2} xs={3}>
-            <img src={weatherCodes[day.values.weatherCode].icon} alt="icon" style={{
-              width: "50px",
-              height: "50px",
-              margin: "-10px -5px"
-            }}/>
+            <img
+              src={weatherCodes[day.values.weatherCode].icon}
+              alt="icon"
+              style={{
+                width: "50px",
+                height: "50px",
+                margin: "-10px -5px",
+              }}
+            />
             {weatherCodes[day.values.weatherCode].desc}
           </Col>
           <Col xs={2}>{day.values.temperatureMax}</Col>
@@ -50,7 +71,9 @@ export default function DayView() {
   )?.intervals;
   return (
     (weeklyWeather && <DayViewTable weeklyWeather={weeklyWeather} />) || (
-      <Alert variant="warning" className="mt-3">No day view data available</Alert>
+      <Alert variant="warning" className="mt-3">
+        No day view data available
+      </Alert>
     )
   );
 }
